15a6f61f88a36634dc82ea1df92d77148a30caee
DinisSimoes <di.f.simoes@gmail.com>
2024-12-22 00:06:33 -0300

    add entities
     - order
     - orderItem

---
+++ Orderly.Consumer/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace Orderly.Consumer.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Completed,
+        Cancelled
+    }
+}
